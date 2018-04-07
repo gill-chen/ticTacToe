f3cefbb98914b6b716e1d4ad09fe5767a83c12be
gillian20993 <gillian.20993@gmail.com>
2018-04-07 10:20:58 -0700

    Changed: Alternates X or O between moves, no more extra clicking

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,16 +1,18 @@
 console.log("CONNECTED");
-//tictactoe for 2 human players
+//tictactoe with Computer Player
 
 var buttons = document.querySelectorAll("td");
 var lock = document.getElementById("lock");
 var attach = new Array();
-
+var alternate; 
 //only runs once at the beginning when code loads
 for (var i = 0, length = buttons.length; i < length; i++)
 	{ 
-		console.log("forloop");
+		//console.log("forloop");
 		//adds play function to each table cell, as well as parameter
 		//that function is attached
+		buttons[i].index = i;
+		//console.log(buttons[i].index);
 		attach[i] = true;
 		buttons[i].addEventListener("click", play);
 	}
@@ -19,42 +21,57 @@ function play(){
 	//for loop to ensure player only enters one icon (x or o)on the board
 	//on each turn
 	var count = 0;
-	for (var i = 0, length = buttons.length; i < length; i++)
-		{ 	
+	for (var i = 0, length = buttons.length; i < length; i++){ 	
 		//this will disable entering an icon for this table cell
        //if this cell you're clicking on is empty, but there's already another
        //icon in a cell not locked yet, it will increase the count so that you can't
        //enter anything in this cell
 			if ((buttons[i].innerHTML != "" && attach[i]) && this.innerHTML == ""){
+				for (var i = 0, length = buttons.length; i < length; i++){
+					if (buttons[i].innerHTML != "" && attach[i] == true){
+						buttons[i].innerHTML = "";
+					}
+				}
 				count++; 
 			}
-		}
+	}
 	//if other table cells are not filled or locked, then player can
 	//enter an icon for this table cell
-	if (count == 0){
-		//allows player to pick between x or o or empty
-		if (this.innerHTML == ""){
+	
+		
+		if (alternate == true){
+			if (this.innerHTML == ""){
 				this.innerHTML = "X";
 			}
 			else if (this.innerHTML == "X"){
+				this.innerHTML = "";
+			}
+		}
+		else {
+			if (this.innerHTML == ""){
 				this.innerHTML = "O";
 			}
 			else if (this.innerHTML == "O"){
 				this.innerHTML = "";
 			}
-	}
+		}
+		
+	
 		
 }
 //this function locks all non-empty table cells by removing play function
 lock.addEventListener("click", function(){
+	alternate = !alternate;
+	console.log(alternate);
 	for (var i = 0, length = buttons.length; i < length; i++){ 
 			if (buttons[i].innerHTML != ""){
 				attach[i] = false;
 				buttons[i].removeEventListener("click", play);
 				}
 		}
-	
+	//do something to fill in empty spot 
 });
+
 
 
 //reset global variables to start game again
