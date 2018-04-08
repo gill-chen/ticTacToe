5f377828cb22df7836b68211e9341a6a56c7336f
gillian20993 <gillian.20993@gmail.com>
2018-04-08 08:56:51 -0700

    Added: CSS animation on icons and cleaned up repetitive code

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,7 +1,7 @@
 console.log("CONNECTED");
 //tictactoe with Computer Player
 
-var buttons = document.querySelectorAll("td");
+var buttons = document.querySelectorAll(".icons");
 var lock = document.getElementById("lock");
 var attach = new Array();
 var alternate; 
@@ -12,53 +12,48 @@ for (var i = 0, length = buttons.length; i < length; i++)
 		//adds play function to each table cell, as well as parameter
 		//that function is attached
 		buttons[i].index = i;
-		//console.log(buttons[i].index);
+		console.log(buttons[i].index);
+		
 		attach[i] = true;
 		buttons[i].addEventListener("click", play);
+		buttons[i].classList.add("pre-animation");
 	}
 
 function play(){
 	//for loop to ensure player only enters one icon (x or o)on the board
 	//on each turn
-	var count = 0;
 	for (var i = 0, length = buttons.length; i < length; i++){ 	
 		//this will disable entering an icon for this table cell
        //if this cell you're clicking on is empty, but there's already another
        //icon in a cell not locked yet, it will increase the count so that you can't
        //enter anything in this cell
-			if ((buttons[i].innerHTML != "" && attach[i]) && this.innerHTML == ""){
-				for (var i = 0, length = buttons.length; i < length; i++){
-					if (buttons[i].innerHTML != "" && attach[i] == true){
-						buttons[i].innerHTML = "";
-					}
-				}
-				count++; 
-			}
+		if ((buttons[i].innerHTML != "" && attach[i]) && this.innerHTML == ""){		
+			buttons[i].classList.add("pre-animation");
+			buttons[i].innerHTML = "";
+			buttons[i].classList.remove("test");
+			//this.classList.add("pre-animation");
+			//this.classList.remove("test");			
+		}
 	}
 	//if other table cells are not filled or locked, then player can
-	//enter an icon for this table cell
-	
-		
-		if (alternate == true){
-			if (this.innerHTML == ""){
-				this.innerHTML = "X";
-			}
-			else if (this.innerHTML == "X"){
-				this.innerHTML = "";
-			}
+	//enter an icon for this table cell	
+	if (alternate == true){
+		if (this.innerHTML == ""){
+			this.innerHTML = "X";
+			this.classList.remove("pre-animation");
+			this.classList.add("test");
+				
+		}			
+	}
+	else {
+		if (this.innerHTML == ""){
+			this.innerHTML = "O";	
+			this.classList.remove("pre-animation");
+			this.classList.add("test");
 		}
-		else {
-			if (this.innerHTML == ""){
-				this.innerHTML = "O";
-			}
-			else if (this.innerHTML == "O"){
-				this.innerHTML = "";
-			}
-		}
-		
-	
-		
+	}
 }
+
 //this function locks all non-empty table cells by removing play function
 lock.addEventListener("click", function(){
 	alternate = !alternate;
@@ -66,6 +61,7 @@ lock.addEventListener("click", function(){
 	for (var i = 0, length = buttons.length; i < length; i++){ 
 			if (buttons[i].innerHTML != ""){
 				attach[i] = false;
+				buttons[i].classList.add("opaque");
 				buttons[i].removeEventListener("click", play);
 				}
 		}
